e1ce75b26a30fd4d59a0c77f6b40c7cc6367c2c0
meinman <einmanjr@gmail.com>
2020-04-05 17:32:25 -0700

    Scoreboard Added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("Easter Egg")] [SerializeField] GameObject deathFX;
+    [Tooltip("Parent to all")] [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 25;
+
+    ScoreBoard scoreBoard;
+
+        void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.scoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);  
+    }
+
+}
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SelfDestructor : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Destroy(gameObject, 5f); // allow customization
+    }
+
+    void Update()
+    {
+        
+    }
+}
